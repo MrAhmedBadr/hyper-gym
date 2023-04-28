687842822394e94d86e08413ac4052119a28b10f
Ahmed Badr <lordahmed217@gmail.com>
2023-04-28 07:35:27 +0300

    update

---
+++ gym.js
@@ -0,0 +1,20 @@
+ let familyNav = document.getElementById('familyNav');
+ let offerNav = document.getElementById('offerNav');
+ let contactNav = document.getElementById('contactNav');
+
+ familyNav.addEventListener('click', ()=>{
+    scrollTo(0 , 600)
+    
+});
+
+offerNav.addEventListener('click', ()=>{
+    scrollTo(0 , 1400)
+    
+});
+
+contactNav.addEventListener('click', ()=>{
+    scrollTo(0 , 2800)
+    
+});
+
+
